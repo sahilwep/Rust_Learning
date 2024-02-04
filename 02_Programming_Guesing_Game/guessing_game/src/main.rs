67f593d25c4d6578131b9d703131ed1048184629
Sahil Sharma <sahilwep@gmail.com>
2024-02-04 15:17:42 +0530

    cheatsheet

--- 02_Programming_Guesing_Game/guessing_game/src/main.rs
+++ 02_Programming_Guesing_Game/guessing_game/src/main.rs
@@ -1,13 +1,14 @@
+fn main(){
 
-fn main() {
-
-  
-    let decimal: f32 = 65.2342;
-
-    // converting integer to char using as keyword
-    let integer = decimal as u32;
-
-    println!("decimal = {}", decimal);
-    println!("integer = {}", integer);
-      
+    let mut num = 0;
+    loop {
+        num += 1;
+        if num == 5 {
+            continue;
+        }
+        if num == 11{
+            break;
+        }
+        print!("{num} ");
+    }
 }
