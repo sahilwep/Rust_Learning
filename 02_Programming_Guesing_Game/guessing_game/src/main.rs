0e7be9a4c5ad5f62e684e56d33869dcba775d7c1
Sahil Sharma <sahilwep@gmail.com>
2024-02-05 13:57:32 +0530

    Cheetsheet

--- 02_Programming_Guesing_Game/guessing_game/src/main.rs
+++ 02_Programming_Guesing_Game/guessing_game/src/main.rs
@@ -1,15 +1,5 @@
 fn main(){
+    
 
-    let mut number = [1,2,3,4,5];
-    
-    println!("number = {:?}", number);
-
-    let slice = &mut number[1..3];
-    
-    println!("slice = {:?}", slice);
-    
-    slice[1] = 99;
-    
-    println!("changed slice = {:?}", slice);
 
 }
