9a0e6a457ee212c122d4dcea2960caa78a1bf40e
Sahil Sharma <sahilwep@gmail.com>
2024-02-14 10:50:16 +0530

    cheatsheet

--- 02_Programming_Guesing_Game/guessing_game/src/main.rs
+++ 02_Programming_Guesing_Game/guessing_game/src/main.rs
@@ -1,14 +1,16 @@
+
+// Function to find a user by their username which returns as Option enum
+fn get_user(username: &str) -> Option<&str> {
+   if username.is_empty() {
+      return None;
+   }
+   return Some(username);
+}
+
 fn main(){
+      // use of unwrap method of get the result of Option enum from get_user function
+      let result = get_user("Sahil").unwrap();
 
-   let text = "Hello, world!";
-
-   let character_option = text.chars().nth(11);
-
-   // using match for Options type
-   let character = match character_option {
-      None => "empty".to_string(),
-      Some(c) => c.to_string()
-   };
-
-   println!("Character at index 11 is {}", character);
+      // print the result 
+      println!("User = {:?}", result);
 }
